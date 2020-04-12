3a17f5fd3f152e45146490dd7f4ad91a9a6e193f
Sergey <sergei.klimushkin@simbirsoft.com>
2020-04-12 19:54:14 +0400

    Some changes

---
+++ test/controllers/application_controller_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class ApplicationControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+
+  test "should get root" do
+    get root_url
+    assert_response :success
+    assert_select "title", "Home | #{@base_title}"
+  end
+end
