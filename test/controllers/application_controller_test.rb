3e28c270ef816e7b317b4fa5812b2e6770025ecc
Sergey Klimushkin <sergei.klimushkin@simbirsoft.com>
2020-04-14 19:12:35 +0400

    Fixed rubocop issues

--- test/controllers/application_controller_test.rb
+++ test/controllers/application_controller_test.rb
@@ -2,12 +2,12 @@ require 'test_helper'
 
 class ApplicationControllerTest < ActionDispatch::IntegrationTest
   def setup
-    @base_title = "Ruby on Rails Tutorial Sample App"
+    @base_title = 'Ruby on Rails Tutorial Sample App'
   end
 
-  test "should get root" do
+  test 'should get root' do
     get root_url
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_select 'title', "Home | #{@base_title}"
   end
 end
