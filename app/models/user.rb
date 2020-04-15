6782d0a27dddd68ac953471419cabbf156ae7173
Sergey Klimushkin <sergei.klimushkin@simbirsoft.com>
2020-04-15 21:54:49 +0400

    Basic user model

---
+++ app/models/user.rb
@@ -0,0 +1,19 @@
+# @!attribute id
+# @!attribute name
+# @!attribute email
+# @!attribute created_at
+# @!attribute updated_at
+# @!attribute password
+# @!attribute password_confirmation
+class User < ApplicationRecord
+  before_save { email.downcase! }
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
+  validates :email,
+            presence: true,
+            length: { maximum: 255 },
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
+end
