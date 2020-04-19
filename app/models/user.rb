a107079cdbf89daa6b26df6e22a862253b0249a5
Sergey Klimushkin <sergei.klimushkin@simbirsoft.com>
2020-04-19 11:49:09 +0400

    tmp

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@
 # @!attribute updated_at
 # @!attribute password
 # @!attribute password_confirmation
+# @!attribute remember_digest
 class User < ApplicationRecord
   before_save { email.downcase! }
   validates :name, presence: true, length: { maximum: 50 }
@@ -16,9 +17,28 @@ class User < ApplicationRecord
             uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+  attr_accessor :remember_token
 
   def self.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine::cost
     BCrypt::Password.create(string, cost: cost)
+  end
+
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 end
